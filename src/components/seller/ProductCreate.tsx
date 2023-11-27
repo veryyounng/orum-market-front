64537973dc944199edf45dc92daf0a79b7ea0e17
veryyounng <veryyounng>
2023-11-27 20:19:38 +0900

    Feat: 상품등록 더미데이터 전송 #12

--- src/components/seller/ProductCreate.tsx
+++ src/components/seller/ProductCreate.tsx
@@ -1,3 +1,52 @@
+import { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Input } from '@mui/material';
+import { api } from '../../api/api';
+
 export default function ProductCreate() {
-  return <div>ProductCreate</div>;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(event.target.value);
+  };
+  const productSubmit = async () => {
+    await api
+      .createProduct()
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      ProductCreate
+      <div>
+        <input
+          type="text"
+          placeholder="상품명을 입력하세요."
+          value={title}
+          onChange={handleTitleChange}
+        ></input>
+      </div>
+      <div>
+        <Input
+          type="text"
+          placeholder="상품 설명을 입력하세요."
+          value={content}
+          onChange={handleContentChange}
+        ></Input>
+      </div>
+      <div>
+        <input type="submit" value="등록하기" onClick={productSubmit}></input>
+      </div>
+    </div>
+  );
 }
