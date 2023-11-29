a8bd2fec09dc003380e8b8e4931c9b1847b829b2
jingoworld <lonul83@gmail.com>
2023-11-29 10:38:03 +0900

    Feat: 장바구니 구현 기능 모듈화 #20

--- src/pages/product/ProductCard.tsx
+++ src/pages/product/ProductCard.tsx
@@ -1,23 +1,40 @@
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { IProduct } from '../../type';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useAddToCart } from '../../hooks/useAddToCart';
 
 interface ProductCardProps {
   product: IProduct;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const handleAddToCart = useAddToCart();
+
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
       <Card>
-        <Link to={`/product/${product._id}`}>
-          <CardMedia
-            component="img"
-            height="140"
-            image={product.mainImages[0]}
-            alt={product.name}
-          />
-        </Link>
+        <Tooltip title="상품 페이지로 이동하기" arrow>
+          <CardActionArea component={Link} to={`/product/${product._id}`}>
+            <CustomCardMedia
+              component="img"
+              height="140"
+              image={product.mainImages[0]}
+              alt={product.name}
+            />
+          </CardActionArea>
+        </Tooltip>
         <CardContent>
           <Link
             to={`/product/${product._id}`}
@@ -33,7 +50,23 @@ export default function ProductCard({ product }: ProductCardProps) {
             Shipping Fees: {product.shippingFees}원
           </Typography>
         </CardContent>
+        <CardActions>
+          <Button
+            startIcon={<ShoppingCartIcon />}
+            onClick={() => handleAddToCart(product)}
+          >
+            장바구니
+          </Button>
+        </CardActions>
       </Card>
     </Grid>
   );
 }
+
+const CustomCardMedia = styled(CardMedia)`
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
