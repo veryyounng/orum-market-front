873f66254ab8670e831f49dab703ff2e5c678b02
wSeungMi <seungmi.dev@gmail.com>
2023-12-06 20:04:58 +0900

    Refactor: 등록된 배송지 없는 경우 출력 페이지 추가 #25

--- src/components/address/AddressForm.tsx
+++ src/components/address/AddressForm.tsx
@@ -1,4 +1,5 @@
-import { TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export interface IUserInfoAddress {
   address: string;
@@ -9,18 +10,44 @@ export default function AddressForm({
   address,
   handleChangeUserAddress,
 }: IUserInfoAddress) {
+  const _id = localStorage.getItem('_id');
   return (
     <>
-      <TextField
-        type="text"
-        placeholder="주소를 입력해주세요"
-        value={address}
-        onChange={(e) => {
-          handleChangeUserAddress(e.target.value);
-        }}
-        size="small"
-        fullWidth
-      />
+      {
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            marginY: '1rem',
+          }}
+          height={'100px'}
+        >
+          <Typography variant="body2">등록된 배송지가 없습니다.</Typography>
+          <Link to={`/user/${_id}/buyer-info/address`}>
+            <Button
+              size={'small'}
+              variant="outlined"
+              sx={{ marginTop: '0.5rem' }}
+            >
+              배송지 등록
+            </Button>
+          </Link>
+        </Box>
+      }
+      {address && (
+        <TextField
+          type="text"
+          placeholder="주소를 입력해주세요"
+          value={address}
+          onChange={(e) => {
+            handleChangeUserAddress(e.target.value);
+          }}
+          size="small"
+          fullWidth
+        />
+      )}
     </>
   );
 }
