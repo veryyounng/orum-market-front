a4dcb2fe0bb60f60682035495c102eaaac75de9e
wSeungMi <seungmi.dev@gmail.com>
2023-12-06 10:15:55 +0900

    Design: MUI sort select box 구현 #52

--- src/components/seller/ProductManager.tsx
+++ src/components/seller/ProductManager.tsx
@@ -10,6 +10,10 @@ import {
   Typography,
   Box,
   Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
 } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import { useEffect, useState } from 'react';
@@ -22,6 +26,7 @@ import { Link } from 'react-router-dom';
 export default function ProductManager() {
   const _id = localStorage.getItem('_id');
   const [productList, setProductList] = useState<IProduct[]>([]);
+  const [sortOrder, setSortOrder] = useState('최신순');
   const [isShow, setIsShow] = useState(false);
 
   // 날짜 변환 함수
@@ -65,96 +70,121 @@ export default function ProductManager() {
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table aria-label="구매내역">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">
-                등록일자
-                <br /> (등록번호)
-              </TableCell>
-              <TableCell align="center">카테고리</TableCell>
-              <TableCell align="center">이미지</TableCell>
-              <TableCell align="center">상품명</TableCell>
-              <TableCell align="center">수량</TableCell>
-              <TableCell align="center">가격</TableCell>
-              <TableCell align="center">공개여부</TableCell>
-              <TableCell align="center"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {productList.map((rows) => (
-              <TableRow key={rows._id}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-end',
+          gap: 2,
+        }}
+      >
+        <FormControl>
+          <InputLabel id="sort-label">정렬</InputLabel>
+          <Select
+            labelId="sort-label"
+            id="sort-select"
+            value={sortOrder}
+            size="small"
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <MenuItem value="최신순">최신순</MenuItem>
+            <MenuItem value="오래된순">오래된순</MenuItem>
+            <MenuItem value="가-하">가격: 낮은 순</MenuItem>
+            <MenuItem value="하-가">가격: 높은 순</MenuItem>
+          </Select>
+        </FormControl>
+        <TableContainer component={Paper}>
+          <Table aria-label="구매내역">
+            <TableHead>
+              <TableRow>
                 <TableCell align="center">
-                  <Typography variant="body2" color="text.secondary">
-                    {formatDate(rows.createdAt)}
-                  </Typography>
-                  ({rows._id})
+                  등록일자
+                  <br /> (등록번호)
                 </TableCell>
-                <TableCell align="center">
-                  {CATEGORY.depth2
-                    .filter(
-                      (category) => category.dbCode === rows.extra.category[1],
-                    )
-                    .map((categoryName) => (
-                      <Typography key={categoryName.id} variant="body2">
-                        {categoryName.name}
-                      </Typography>
-                    ))}
-                </TableCell>
-                <TableCell align="center">
-                  <img
-                    src={`${rows.mainImages[0]}`}
-                    alt="main-Image"
-                    style={{ width: '80px' }}
-                  />
-                </TableCell>
-                <TableCell align="center">{rows.name}</TableCell>
-                <TableCell align="center">{rows.quantity}</TableCell>
-                <TableCell align="center">
-                  {rows.price.toLocaleString()}원
-                </TableCell>
-                <TableCell align="center">
-                  <ToggleButton
-                    value="check"
-                    selected={isShow}
-                    size={'small'}
-                    onChange={() => {
-                      setIsShow(!rows.show);
-                    }}
-                  >
-                    <CheckIcon />
-                  </ToggleButton>
-                </TableCell>
-                <TableCell align="center">
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                      gap: 1,
-                    }}
-                  >
-                    <Link to={`/product/${rows._id}`}>
-                      <Button type="button" variant="contained">
-                        상세보기
-                      </Button>
-                    </Link>
-                    <Link
-                      to={`/user/${rows._id}/product-update`}
-                      state={{ productId: `${rows._id}` }}
-                    >
-                      <Button type="button" variant="contained">
-                        수정하기
-                      </Button>
-                    </Link>
-                  </Box>
-                </TableCell>
+                <TableCell align="center">카테고리</TableCell>
+                <TableCell align="center">이미지</TableCell>
+                <TableCell align="center">상품명</TableCell>
+                <TableCell align="center">수량</TableCell>
+                <TableCell align="center">가격</TableCell>
+                <TableCell align="center">공개여부</TableCell>
+                <TableCell align="center"></TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {productList.map((rows) => (
+                <TableRow key={rows._id}>
+                  <TableCell align="center">
+                    <Typography variant="body2" color="text.secondary">
+                      {formatDate(rows.createdAt)}
+                    </Typography>
+                    ({rows._id})
+                  </TableCell>
+                  <TableCell align="center">
+                    {CATEGORY.depth2
+                      .filter(
+                        (category) =>
+                          category.dbCode === rows.extra.category[1],
+                      )
+                      .map((categoryName) => (
+                        <Typography key={categoryName.id} variant="body2">
+                          {categoryName.name}
+                        </Typography>
+                      ))}
+                  </TableCell>
+                  <TableCell align="center">
+                    <img
+                      src={`${rows.mainImages[0]}`}
+                      alt="main-Image"
+                      style={{ width: '80px' }}
+                    />
+                  </TableCell>
+                  <TableCell align="center">{rows.name}</TableCell>
+                  <TableCell align="center">{rows.quantity}</TableCell>
+                  <TableCell align="center">
+                    {rows.price.toLocaleString()}원
+                  </TableCell>
+                  <TableCell align="center">
+                    <ToggleButton
+                      value="check"
+                      selected={isShow}
+                      size={'small'}
+                      onChange={() => {
+                        setIsShow(!rows.show);
+                      }}
+                    >
+                      <CheckIcon />
+                    </ToggleButton>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: 1,
+                      }}
+                    >
+                      <Link to={`/product/${rows._id}`}>
+                        <Button type="button" variant="contained">
+                          상세보기
+                        </Button>
+                      </Link>
+                      <Link
+                        to={`/user/${rows._id}/product-update`}
+                        state={{ productId: `${rows._id}` }}
+                      >
+                        <Button type="button" variant="contained">
+                          수정하기
+                        </Button>
+                      </Link>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </>
   );
 }
