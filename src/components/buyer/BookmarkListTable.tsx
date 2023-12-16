be8c3e26dc467569f7d4960282886189a2727d1b
wSeungMi <seungmi.dev@gmail.com>
2023-12-16 22:00:31 +0900

    Refactor: BookmarkListTable Fragment 삭제 #86

--- src/components/buyer/BookmarkListTable.tsx
+++ src/components/buyer/BookmarkListTable.tsx
@@ -15,37 +15,35 @@ import deleteBookmark from '../../lib/deleteBookmark';
 
 export default function BookmarkListTable({ myBookmarkList }) {
   return (
-    <>
-      <Container>
-        {myBookmarkList
-          .map((bookmark) => (
-            <StyledCard key={bookmark._id}>
-              <CardActionArea
-                component={Link}
-                to={`/product/${bookmark.product_id}`}
-              >
-                <ProductImage
-                  image={bookmark.product.image.path}
-                  title={bookmark.product.name}
-                />
-                <ProductDetails>
-                  <Typography variant="h6">{bookmark.product.name}</Typography>
-                  <Typography variant="h6" color="inherit" fontWeight={700}>
-                    {bookmark.product.price.toLocaleString()} 원
-                  </Typography>
-                </ProductDetails>
-              </CardActionArea>
-              <ProductActions>
-                <IconButton onClick={() => deleteBookmark(bookmark._id)}>
-                  <FavoriteIcon />
-                </IconButton>
-              </ProductActions>
-            </StyledCard>
-          ))
-          .sort()
-          .reverse()}
-      </Container>
-    </>
+    <Container>
+      {myBookmarkList
+        .map((bookmark) => (
+          <StyledCard key={bookmark._id}>
+            <CardActionArea
+              component={Link}
+              to={`/product/${bookmark.product_id}`}
+            >
+              <ProductImage
+                image={bookmark.product.image.path}
+                title={bookmark.product.name}
+              />
+              <ProductDetails>
+                <Typography variant="h6">{bookmark.product.name}</Typography>
+                <Typography variant="h6" color="inherit" fontWeight={700}>
+                  {bookmark.product.price.toLocaleString()} 원
+                </Typography>
+              </ProductDetails>
+            </CardActionArea>
+            <ProductActions>
+              <IconButton onClick={() => deleteBookmark(bookmark._id)}>
+                <FavoriteIcon />
+              </IconButton>
+            </ProductActions>
+          </StyledCard>
+        ))
+        .sort()
+        .reverse()}
+    </Container>
   );
 }
 
