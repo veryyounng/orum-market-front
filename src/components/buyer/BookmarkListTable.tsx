2f99639791d9941e9a06c10bfede311c3dea9324
wSeungMi <seungmi.dev@gmail.com>
2023-12-17 20:34:02 +0900

    Design: 찜한 상품 정렬 수정 #87

--- src/components/buyer/BookmarkListTable.tsx
+++ src/components/buyer/BookmarkListTable.tsx
@@ -1,49 +1,62 @@
 import {
   CardActionArea,
-  Container,
   Typography,
-  IconButton,
   styled,
   Card,
   CardMedia,
   Box,
+  Grid,
+  Button,
 } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import { Link } from 'react-router-dom';
 
 import deleteBookmark from '../../lib/deleteBookmark';
+import { Close } from '@mui/icons-material';
 
 export default function BookmarkListTable({ myBookmarkList }) {
+  // 아이템 사이즈를 계산하는 함수
+  const getItemSize = () => {
+    return { xs: 12, sm: 6, md: 4, lg: 4, xl: 4 };
+  };
   return (
-    <Container>
+    <>
       {myBookmarkList
         .map((bookmark) => (
-          <StyledCard key={bookmark._id}>
-            <CardActionArea
-              component={Link}
-              to={`/product/${bookmark.product_id}`}
-            >
-              <ProductImage
-                image={bookmark.product.image.path}
-                title={bookmark.product.name}
-              />
-              <ProductDetails>
-                <Typography variant="h6">{bookmark.product.name}</Typography>
-                <Typography variant="h6" color="inherit" fontWeight={700}>
-                  {bookmark.product.price.toLocaleString()} 원
-                </Typography>
-              </ProductDetails>
-            </CardActionArea>
-            <ProductActions>
-              <IconButton onClick={() => deleteBookmark(bookmark._id)}>
-                <FavoriteIcon />
-              </IconButton>
-            </ProductActions>
-          </StyledCard>
+          <Grid item {...getItemSize()} key={bookmark._id} marginTop={3}>
+            <StyledCard>
+              <CardActionArea
+                component={Link}
+                to={`/product/${bookmark.product_id}`}
+              >
+                <ProductImage
+                  image={bookmark.product.image.path}
+                  title={bookmark.product.name}
+                />
+                <ProductDetails>
+                  <Typography variant="h6">{bookmark.product.name}</Typography>
+                  <Typography variant="h6" color="inherit" fontWeight={700}>
+                    {bookmark.product.price.toLocaleString()} 원
+                  </Typography>
+                </ProductDetails>
+              </CardActionArea>
+              <ProductActions>
+                <Button variant="outlined" style={{ flexGrow: '1' }}>
+                  장바구니 담기
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => deleteBookmark(bookmark._id)}
+                >
+                  <Close />
+                  삭제
+                </Button>
+              </ProductActions>
+            </StyledCard>
+          </Grid>
         ))
         .sort()
         .reverse()}
-    </Container>
+    </>
   );
 }
 
@@ -53,6 +66,7 @@ const StyledCard = styled(Card)({
   position: 'relative',
   boxShadow: 'none',
   borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+  paddingBottom: '6px',
 });
 
 const ProductImage = styled(CardMedia)({
@@ -65,12 +79,14 @@ const ProductDetails = styled(Box)({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'start',
-  padding: '16px',
+  padding: '12px 4px 6px',
 });
 
 const ProductActions = styled(Box)({
   display: 'flex',
-  justifyContent: 'flex-end',
+  justifyContent: 'space-between',
+  gap: '6px',
   alignItems: 'center',
   padding: '4px',
+  flexWrap: 'wrap',
 });
--- src/components/buyer/BuyerFavorite.tsx
+++ src/components/buyer/BuyerFavorite.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import BookmarkListTable from './BookmarkListTable';
 import useGetBookmark from '../../hooks/useGetBookmark';
@@ -9,7 +9,10 @@ export default function BuyerFavorite() {
   if (!bookmarkList || bookmarkList?.length === 0) {
     return (
       <>
-        <Typography variant="h6">북마크된 상품이 없습니다.</Typography>
+        <Typography variant="h5" fontWeight={700}>
+          찜한 상품 ({bookmarkList.length})
+        </Typography>
+        <Typography variant="h6">찜한 상품 목록이 없습니다.</Typography>
         <Link to={`/`}>
           <Button type="button" variant="outlined" size="medium">
             상품 보러 가기
@@ -22,9 +25,11 @@ export default function BuyerFavorite() {
   return (
     <>
       <Typography variant="h5" fontWeight={700}>
-        찜한 상품
+        찜한 상품 ({bookmarkList.length})
       </Typography>
-      <BookmarkListTable myBookmarkList={bookmarkList} />
+      <Grid container spacing={4} rowSpacing={4}>
+        <BookmarkListTable myBookmarkList={bookmarkList} />
+      </Grid>
     </>
   );
 }
