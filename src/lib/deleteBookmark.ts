7feacda6ea5748ffa6f277ff204f9c5393fa9020
wSeungMi <seungmi.dev@gmail.com>
2023-12-17 18:01:34 +0900

    Refactor: 찜한 상품 삭제시 안내 문구 수정 #87

--- src/lib/deleteBookmark.ts
+++ src/lib/deleteBookmark.ts
@@ -1,15 +1,16 @@
 import { api } from '../api/api';
 
 export default async function deleteBookmark(bookmarkId: number) {
-  const confirmRemoveBookmark = window.confirm('북마크를 삭제하시겠습니까?');
+  const confirmRemoveBookmark =
+    window.confirm('찜한 상품에서 삭제하시겠습니까?');
 
   if (confirmRemoveBookmark) {
     try {
       await api.removeBookmark(Number(bookmarkId));
-      alert('bookmark 삭제 성공');
+      alert('찜한 상품에서 삭제되었습니다.');
       window.location.reload();
     } catch (error) {
-      console.log('북마크 삭제에 실패했습니다.');
+      console.log('찜한 상품 삭제에 실패했습니다.');
     }
   }
 }
