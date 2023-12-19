f1867426873a421053348c3ce78f8c2561ef9a83
wSeungMi <seungmi.dev@gmail.com>
2023-12-19 18:50:39 +0900

    Design: 내 주문 내역 반응형 작업 및 카드 디자인 작업 #94

--- src/components/buyer/OrderListTable.tsx
+++ src/components/buyer/OrderListTable.tsx
@@ -8,167 +8,205 @@ import {
   Button,
   Typography,
   Table,
-  Grid,
+  useMediaQuery,
+  createTheme,
+  ThemeProvider,
   Card,
-  styled,
   Box,
   CardMedia,
-  CardActionArea,
+  CardContent,
 } from '@mui/material';
 import formatDate from '../../lib/formatDate';
 import { ORDER_STATE } from '../../constants';
-import { useEffect } from 'react';
-import { api } from '../../api/api';
-import { Link } from 'react-router-dom';
-import { ChevronRight, Close } from '@mui/icons-material';
+import { useTheme } from '@emotion/react';
+import { ChevronRight } from '@mui/icons-material';
+import styled from 'styled-components';
 
 export default function OrderListTable({ orderList }) {
-  console.log('orderList', orderList);
+  console.log('주문내역', orderList);
 
-  useEffect(() => {
-    const test = async (ttt) => {
-      const response = await api.getOrderProductDetail(ttt);
-      console.log('받아온 값', response.data.item);
-    };
-    test(3);
-  }, []);
+  function ResponseWidth() {
+    const theme: any = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('md'));
 
-  const getItemSize = () => {
-    return { xs: 12, sm: 12, md: 12, lg: 12, xl: 12 };
-  };
+    return (
+      <>
+        {matches ? (
+          <>
+            <TableContainer component={Paper}>
+              <Table aria-label="결제내역">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">
+                      결제일
+                      <br /> (주문번호)
+                    </TableCell>
+                    <TableCell align="center">상품명</TableCell>
+                    <TableCell align="center">수량</TableCell>
+                    <TableCell align="center">
+                      총 결제금액 <br />
+                      (배송비)
+                    </TableCell>
+                    <TableCell align="center">주문처리상태</TableCell>
+                    <TableCell align="center"></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {orderList.map((rows) => (
+                    <TableRow key={rows._id}>
+                      <TableCell align="center">
+                        <Typography variant="body2" color="text.secondary">
+                          {formatDate(rows.createdAt)}
+                        </Typography>
+                        ({rows._id})
+                      </TableCell>
+                      <TableCell align="center">
+                        {rows.products[0].name} 포함 총 {rows.products.length}건
+                      </TableCell>
+                      <TableCell align="center">
+                        {rows.products.length}
+                      </TableCell>
+                      <TableCell align="center">
+                        {rows.cost.total.toLocaleString()}원<br /> (
+                        {rows.cost.shippingFees.toLocaleString()}원)
+                      </TableCell>
+                      <TableCell align="center">
+                        {ORDER_STATE.codes
+                          .filter((state) => state.code === rows.state)
+                          .map((stateValue) => (
+                            <Typography key={stateValue.code} variant="body2">
+                              {stateValue.value}
+                            </Typography>
+                          ))}
+                      </TableCell>
+                      <TableCell align="center">
+                        <Button type="button" variant="outlined">
+                          별점평가
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        ) : (
+          <>
+            {orderList.map((list) => (
+              <Card sx={{ marginBottom: '20px' }}>
+                <OrderListBox>
+                  <Typography variant="body1" fontWeight={700}>
+                    {formatDate(list.createdAt)}
+                  </Typography>
+                  <Button sx={{ padding: '0', margin: '0' }}>
+                    상세보기 <ChevronRight />
+                  </Button>
+                </OrderListBox>
+                {list.products.map((product) => (
+                  <OrderProductList>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      style={{ width: '200px', objectFit: 'cover' }}
+                      image={product.image.path}
+                      alt={product.name}
+                      sx={{ padding: '15px' }}
+                    />
+                    <CardContent
+                      sx={{
+                        padding: '15px 0 0 0 ',
+                        margin: '0',
+                      }}
+                    >
+                      <Typography
+                        variant="subtitle2"
+                        fontStyle={{ color: '#646464' }}
+                      >
+                        주문번호: {product._id}
+                      </Typography>
+                      <Typography
+                        variant="body1"
+                        fontWeight={700}
+                        textOverflow={'ellipsis'}
+                        marginTop={0.3}
+                      >
+                        {product.name}
+                      </Typography>
+                      <Typography variant="body1" marginTop={0.5}>
+                        {product.price.toLocaleString()}원
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        marginTop={1.5}
+                        fontStyle={{ color: 'red' }}
+                      >
+                        {ORDER_STATE.codes
+                          .filter((state) => state.code === list.state)
+                          .map((stateValue) => (
+                            <>
+                              <Typography
+                                key={stateValue.code}
+                                variant="body2"
+                                marginBottom={1}
+                              >
+                                {stateValue.value}
+                              </Typography>
+                              <OrderReviewBox>
+                                {stateValue.value === '배송 완료' ? (
+                                  <Button
+                                    variant="outlined"
+                                    size="small"
+                                    fullWidth
+                                  >
+                                    별점평가
+                                  </Button>
+                                ) : (
+                                  ''
+                                )}
+                              </OrderReviewBox>
+                            </>
+                          ))}
+                      </Typography>
+                    </CardContent>
+                  </OrderProductList>
+                ))}
+              </Card>
+            ))}
+          </>
+        )}
+      </>
+    );
+  }
+
+  const theme = createTheme();
 
   return (
     <>
-      <Grid container sx={{ backgroundColor: 'red' }}>
-        <Grid item {...getItemSize()} marginY={3}>
-          <StyledCard>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'end',
-              }}
-            >
-              <Typography
-                variant="h6"
-                fontWeight={600}
-                marginTop={2}
-                marginBottom={1}
-              >
-                12월 18일 주문
-              </Typography>
-              <Link to={`/`}>
-                <Button type="button" sx={{ padding: '10px 0' }}>
-                  주문내역 더보기 <ChevronRight />
-                </Button>
-              </Link>
-            </Box>
-            <ProductImage />
-            <ProductDetails>
-              <Typography variant="h6">ddd</Typography>
-              <Typography variant="h6" color="inherit" fontWeight={700}>
-                ddd 원
-              </Typography>
-            </ProductDetails>
-            <ProductActions>
-              <Button variant="outlined" style={{ flexGrow: '1' }}>
-                장바구니 담기
-              </Button>
-              <Button variant="outlined">
-                <Close />
-                삭제
-              </Button>
-            </ProductActions>
-          </StyledCard>
-        </Grid>
-      </Grid>
-
-      <TableContainer component={Paper}>
-        <Table aria-label="결제내역">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">
-                결제일
-                <br /> (주문번호)
-              </TableCell>
-              <TableCell align="center">상품명</TableCell>
-              <TableCell align="center">수량</TableCell>
-              <TableCell align="center">
-                총 결제금액 <br />
-                (배송비)
-              </TableCell>
-              <TableCell align="center">주문처리상태</TableCell>
-              <TableCell align="center"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orderList.map((rows) => (
-              <TableRow key={rows._id}>
-                <TableCell align="center">
-                  <Typography variant="body2" color="text.secondary">
-                    {formatDate(rows.createdAt)}
-                  </Typography>
-                  ({rows._id})
-                </TableCell>
-                <TableCell align="center">
-                  {rows.products[0].name} 포함 총 {rows.products.length}건
-                </TableCell>
-                <TableCell align="center">{rows.products.length}</TableCell>
-                <TableCell align="center">
-                  {rows.cost.total.toLocaleString()}원<br /> (
-                  {rows.cost.shippingFees.toLocaleString()}원)
-                </TableCell>
-                <TableCell align="center">
-                  {ORDER_STATE.codes
-                    .filter((state) => state.code === rows.state)
-                    .map((stateValue) => (
-                      <Typography key={stateValue.code} variant="body2">
-                        {stateValue.value}
-                      </Typography>
-                    ))}
-                </TableCell>
-                <TableCell align="center">
-                  <Button type="button" variant="outlined">
-                    별점평가
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <ThemeProvider theme={theme}>
+        <ResponseWidth />
+      </ThemeProvider>
     </>
   );
 }
 
-const StyledCard = styled(Card)({
+const OrderListBox = styled(Box)({
   display: 'flex',
-  flexDirection: 'column',
-  position: 'relative',
-  boxShadow: 'none',
-  borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-  paddingBottom: '6px',
-  border: '0.5px solid #efefef',
-});
-
-const ProductImage = styled(CardMedia)({
-  height: '150px',
-  backgroundSize: 'cover',
-  backgroundColor: 'grey.50',
-});
-
-const ProductDetails = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'start',
-  padding: '12px 4px 6px',
-});
-
-const ProductActions = styled(Box)({
-  display: 'flex',
-  justifyContent: 'space-between',
-  gap: '6px',
   alignItems: 'center',
-  padding: '4px',
-  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+  padding: '15px',
+  borderBottom: '1px solid #e2e2e2',
+});
+
+const OrderProductList = styled(Box)({
+  display: 'flex',
+  alignItems: 'flex-start',
+  justifyContent: 'flex-start',
+  borderBottom: '1px solid #e2e2e2',
+});
+
+const OrderReviewBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingRight: '10px',
 });
