bb1b13bf22e393f19ba85760d63bbda4c85d1c2a
jingoworld <lonul83@gmail.com>
2023-12-24 04:36:46 +0900

    Feat: 툴팁 구현 #116

---
+++ src/components/CustomTooltip.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Tooltip, { TooltipProps } from '@mui/material/Tooltip';
+
+interface ReusableTooltipProps {
+  title: string;
+  children: React.ReactElement;
+  placement?: TooltipProps['placement'];
+  arrow?: boolean;
+}
+
+const ReusableTooltip: React.FC<ReusableTooltipProps> = ({
+  title,
+  children,
+  placement = 'bottom',
+  arrow = false,
+}) => {
+  return (
+    <Tooltip title={title} placement={placement} arrow={arrow}>
+      {children}
+    </Tooltip>
+  );
+};
+
+export default ReusableTooltip;
