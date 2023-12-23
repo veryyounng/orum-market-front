b2e7da5d99bc512c1f32dfa8e4760b3693cb0a0d
jingoworld <lonul83@gmail.com>
2023-12-24 05:27:45 +0900

    Feat: 커스텀 백드랍 구현 #116

---
+++ src/components/CustomBackdrop.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+interface LoadingBackdropProps {
+  open: boolean;
+  handleClose?: () => void;
+}
+
+const LoadingBackdrop: React.FC<LoadingBackdropProps> = ({
+  open,
+  handleClose,
+}) => {
+  return (
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={open}
+      onClick={handleClose}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+};
+
+export default LoadingBackdrop;
